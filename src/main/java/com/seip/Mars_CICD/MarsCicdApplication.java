e6ff5d0ac31aa2b0084d52d145c770e1b1ca0926
Stratos Delis <stratosdelis21@gmail.com>
2025-04-02 16:31:30 +0300

    Initial commit using Spring Initializr

---
+++ src/main/java/com/seip/Mars_CICD/MarsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.seip.Mars_CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seip/Mars_CICD/MarsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seip.Mars_CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
