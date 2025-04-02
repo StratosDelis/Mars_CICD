347de02df4a36c4a6f7a85a4809d1289dd2311b2
Stratos Delis <stratosdelis21@gmail.com>
2025-04-02 17:12:06 +0300

    Initialized Resource class under the model package

---
+++ src/main/java/com/seip/Mars_CICD/model/Resource.java
@@ -0,0 +1,10 @@
+package com.seip.Mars_CICD.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Resource {
+    
+}
