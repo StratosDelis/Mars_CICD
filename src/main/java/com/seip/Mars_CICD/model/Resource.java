c47990ed1dd7bc0decd0588b5e45ca9415ed7edf
Stratos Delis <stratosdelis21@gmail.com>
2025-04-02 17:19:40 +0300

    Set the Resource class as Entity and added ID annotator and getters and setters

--- src/main/java/com/seip/Mars_CICD/model/Resource.java
+++ src/main/java/com/seip/Mars_CICD/model/Resource.java
@@ -5,6 +5,70 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Resource {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String planet_of_origin;
+
+    private double price;
+
+    public Resource() {}
+
+    public Resource(Long id, String name, String description, String planet_of_origin, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.planet_of_origin = planet_of_origin;
+        this.price = price;
+    }
+
+    //Here start all the Setters and Getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPlanet_of_origin() {
+        return planet_of_origin;
+    }
+
+    public void setPlanet_of_origin(String planet_of_origin) {
+        this.planet_of_origin = planet_of_origin;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
